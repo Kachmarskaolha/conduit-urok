27db0e54a766ba3f879424b34925cf2f4d2a5b16
Kachmarska Olha <72305196+Kachmarskaolha@users.noreply.github.com>
2021-02-09 16:10:20 +0200

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import { AuthComponent } from './auth/auth.component';
+import { EditorComponent } from './editor/editor.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SettingsComponent } from './settings/settings.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    AuthComponent,
+    EditorComponent,
+    HomeComponent,
+    ProfileComponent,
+    SettingsComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/models/article-list-config.spec.ts
@@ -0,0 +1,7 @@
+import { ArticleListConfig } from './article-list-config';
+
+describe('ArticleListConfig', () => {
+  it('should create an instance', () => {
+    expect(new ArticleListConfig()).toBeTruthy();
+  });
+});
